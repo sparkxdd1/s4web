d2e21033c76eb3f63cd5c94796597c7de0cc2325
sparkxdd1 <xomrwhite@gmail.com>
2024-07-10 22:59:56 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,67 @@
+
+
+//btns
+
+function donate() {
+    window.open("https://www.donationalerts.com/r/spark_aw");
+    alert ("Thank you for your support!");
+}
+
+function discord() {
+    window.open("https://discord.gg/EBfUXwYz3E")
+}
+
+function steam() {
+    window.open("https://steamcommunity.com/profiles/76561199406960316/")
+}
+
+// home page
+
+
+function arcWarden(){
+    let heroImg = document.getElementById("hero-img");
+    heroImg.src = "images/arc_warden.png";
+    heroImg.classList.remove("brooda");
+
+
+    let heroName = document.getElementById("heroName");
+    heroName.innerHTML = "Arc Warden";
+    
+    let heroDesc = document.getElementById("heroDesc");
+    heroDesc.innerHTML = "Arc Warden is a ranged melee hero who uses a shadowy aura to blind and incapacitate his enemies.";
+}
+
+function shadowFiend(){
+    let heroImg = document.getElementById("hero-img");
+    heroImg.classList.remove("brooda");
+    heroImg.src = "images/nevermore.png";
+
+    let heroName = document.getElementById("heroName");
+    heroName.innerHTML = "Shadow Fiend";
+
+    let heroDesc = document.getElementById("heroDesc");
+    heroDesc.innerHTML = "Shadow Fiend is a ranged melee hero who uses a shadowy aura to blind and incapacitate his enemies.";
+    
+}
+
+function broodmother(){
+
+    let heroImg = document.getElementById("hero-img");
+    heroImg.src = "images/broodmother.png";
+    heroImg.classList.add("brooda");
+
+    let heroName = document.getElementById("heroName");
+    heroName.innerHTML = "Broodmother";
+
+    let heroDesc = document.getElementById("heroDesc");
+    heroDesc.innerHTML = "Broodmother is a melee hero who spreads out of control of her broodlings to attack her enemies.";
+}
+
+
+// projects page
+
+function projectOne(){
+}
+
+function projectTwo(){
+}
